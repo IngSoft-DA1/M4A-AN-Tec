0266d7b6c45cf3da7ff3657f05c4b24c8f6e419e
Ignacio Azaretto <ignacioazaso@gmail.com>
2024-08-29 09:52:38 -0300

    Introducción estructuras basicas

---
+++ proyects/ConsoleApp1/ConsoleAppWithMain/Program.cs
@@ -0,0 +1,25 @@
+﻿using Domain;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+         * 1. Crear solución (Console App)
+         * 2. Crear proyecto de Domain (u otro)
+         * 3. Agregar una clase, via proyecto, o via mismo proyecto de progam.
+         * 4. Instanciar Clase creada en Program.cs
+         */
+        
+        // Ejemplo de instancia
+        User user = new User();
+        
+        string message = "Hello, World!";
+
+        
+        
+        Console.WriteLine(message);
+    }
+}
---
+++ proyects/ConsoleApp1/Domain/User.cs
@@ -0,0 +1,40 @@
+﻿namespace Domain;
+
+public class User
+{
+    private string _name;
+    private DateTime _birthDate;
+    
+    private string Name
+    {
+        get => this._name;
+        set
+        {
+            // Validaciones
+            if (String.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Name must not be null.");
+            }
+            _name = value;
+        }
+    }
+
+    public DateTime BirthDate
+    {
+        get => _birthDate;
+        set => _birthDate = value;
+    }
+    
+    public int Age
+    {
+        get
+        {
+            var today = DateTime.Today;
+            var age = today.Year - _birthDate.Year;
+            if (_birthDate.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+    
+    private string Ci { get; }
+}
